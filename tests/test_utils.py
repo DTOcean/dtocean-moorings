0fd8af7a074292f2023ac69f5148884148ed1cb6
Mathew Topper <damm_horse@yahoo.co.uk>
2017-06-30 13:52:43 +0100

    Add CI files and pandas as dependency

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,12 +2,13 @@
 """
 Created on Sun Mar 06 20:15:41 2016
 
-@author: Winston
+@author: Mathew Topper
 """
 
 import pandas as pd
 
 from dtocean_moorings.utils import dummy_to_dataframe, dataframe_to_dummy
+
 
 def test_dummy_to_dataframe():
     
@@ -34,16 +35,19 @@ def test_dummy_to_dataframe():
     assert isinstance(result, pd.DataFrame)
     assert 'cost' in result.columns
 
+
 def test_dataframe_to_dummy():
     
     dummy_dict = {'cost': {'id1': 37.5, 'id2': 30.5},
                   'item1': {'id1': 'mooring system', 'id2': 'mooring system'},
                   'item2': {'id1': 'chain', 'id2': 'shackle'},
-                  'item3': {'id1': 'studlink chain', 'id2': 'safety bow shackle'},
+                  'item3': {'id1': 'studlink chain',
+                            'id2': 'safety bow shackle'},
                   'item4': {'id1': 'grade 1', 'id2': 'g-2140'},
                   'mbl': {'id1': 417000.0, 'id2': 883000.0},
                   'size': {'id1': 32.0, 'id2': 32.0},
-                  'weight': {'id1': 23.899999999999999, 'id2': 6.4000000000000004}}
+                  'weight': {'id1': 23.899999999999999,
+                             'id2': 6.4000000000000004}}
     df = pd.DataFrame(dummy_dict)
     
     result = dataframe_to_dummy(df)
