d8f798950fbb6acd1a8c2aebe4f53564febfffe7
Mathew Topper <damm_horse@yahoo.co.uk>
2021-08-11 12:55:11 +0100

    Update setup files and set version to 2.1.dev1

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
-
 from distutils.cmd import Command
+
+import yaml
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
+
 
 class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
@@ -22,7 +26,8 @@ class PyTest(TestCommand):
         import pytest
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
-        
+
+
 class CleanPyc(Command):
 
     description = 'clean *.pyc'
@@ -60,13 +65,34 @@ class CleanPyc(Command):
                     continue
                 yield os.path.join(root, fname)
 
+
+def read_yaml(rel_path):
+    with open(rel_path, 'r') as stream:
+        data_loaded = yaml.safe_load(stream)
+    return data_loaded
+
+
+def get_appveyor_version():
+    
+    data = read_yaml("appveyor.yml")
+    
+    if "version" not in data:
+        raise RuntimeError("Unable to find version string.")
+    
+    appveyor_version = data["version"]
+    last_dot_idx = appveyor_version.rindex(".")
+    
+    return appveyor_version[:last_dot_idx]
+
+
 setup(name='dtocean-moorings',
-      version='2.0.0',
-      packages=find_packages(),
+      version=get_appveyor_version(),
       description='The mooring and foundations module for the DTOcean tools',
       maintainer='Mathew Topper',
       maintainer_email='mathew.topper@dataonlygreater.com',
       license="GPLv3",
+      packages=find_packages(),
+      setup_requires=['pyyaml'],
       install_requires=['numpy',
                         'pandas',
                         'polite>=0.9',
@@ -81,4 +107,3 @@ setup(name='dtocean-moorings',
                   'cleanpyc': CleanPyc,
                   }
       )
-       
